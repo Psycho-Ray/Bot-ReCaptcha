ba5257f91f7fa56ea02f088eee1798a8209640d4
Alex Sander <alex.sander@botcity.com.br>
2022-01-14 15:36:35 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ReCaptcha Bot
+
+This is an example of a bot that solves a reCaptcha
+
---
+++ botReCaptcha/bot.py
@@ -0,0 +1,43 @@
+import os
+
+from botcity.web import WebBot, Browser
+
+from botcity.plugins.captcha import BotAntiCaptchaPlugin
+
+
+class Bot(WebBot):
+    def action(self, execution=None):
+        # Init
+        self.headless = False
+        self.driver_path = "./chromedriver.exe"
+
+        # Disable these comments to use Firefox instead
+        # self.driver_path = "./geckodriver.exe"
+        # self.browser = Browser.FIREFOX
+
+        # Opens the BotCity website.
+        self.browse("https://www.google.com/recaptcha/api2/demo")
+
+        # Anti-Captcha - ReCaptcha
+        captcha_id = self.find_element('.g-recaptcha').get_attribute('data-sitekey')
+        anti_captcha = BotAntiCaptchaPlugin(os.getenv("ANTICAPTCHA_KEY"))
+        response = anti_captcha.solve_re("https://www.google.com/recaptcha/api2/demo", captcha_id)
+
+        # Injects the response into the webpage
+        self.execute_javascript('document.getElementById("g-recaptcha-response").innerHTML = "%s"' % response)
+
+        # Clicks submit
+        self.find('submit')
+        self.click()
+
+        # Checks if it worked
+        assert self.find('uhu')
+        print("Success!")
+
+        # Stop the browser and clean up
+        self.wait(3000)
+        self.stop_browser()
+
+
+if __name__ == '__main__':
+    Bot.main()
